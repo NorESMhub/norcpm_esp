dbd0b35566189c2137be390257aee7e0615025a9
Ping-Gin Chiu <Ping-Gin.Chiu@uib.no>
2025-09-10 11:05:16 +0200

    Add noresm 2.0.x install script

---
+++ doc/INSTALL_noresm2.0.x.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+noresm_version=2.0.9
+if [ ! -f "release-noresm$noresm_version.tar.gz" ];then
+    wget -c https://github.com/NorESMhub/NorESM/archive/refs/tags/release-noresm$noresm_version.tar.gz
+fi
+if [ ! -d "NorESM-release-noresm$noresm_version" ];then
+    tar xzf release-noresm$noresm_version.tar.gz
+fi
+cd NorESM-release-noresm$noresm_version/
+
+## necessary patch
+### Externals.cfg, include norcpm as a component
+if [ -z "$(grep norcpm Externals.cfg)" ]; then
+    cat >> Externals.cfg << EOF
+[norcpm]
+tag = n2.0.8.4
+protocol = git
+repo_url = https://github.com/NorESMhub/NorCPM_ESP
+local_path = components/norcpm
+required = true
+EOF
+fi
+
+## download components 
+manage_externals/checkout_externals
+
+### cime/config/cesm/config_files.xml
+if [ -z "$(grep norcpm cime/config/cesm/config_files.xml)" ]; then
+    sed -i -e'219i<value component="norcpm"    >$SRCROOT/components/norcpm</value>' cime/config/cesm/config_files.xml
+fi
+### cime_config/config_compsets.xml
+if [ -z "$(grep norcpm cime_config/config_compsets.xml)" ]; then
+    sed -i -e'559i<!-- NorCPM as esp HIST compset. Add this compset -->\
+  <compset>\
+    <alias>NHISTNCPM</alias>\
+    <lname>HIST_CAM60%NORESM_CLM50%BGC-CROP_CICE%NORESM-CMIP6_BLOM%ECO_MOSART_SGLC_SWAV_NORCPM_BGC%BDRDDMS</lname>\
+  </compset>\
+  ' \
+    cime_config/config_compsets.xml
+fi
+
+### patch for BLOM multi instances
+if [ -z "$(grep '        pg_blom = OcnInParamGen.from_namelist_xml(xml_fil)' components/blom//cime_config/buildnml)" ]; then
+    sed -i -e'116i\        pg_blom = OcnInParamGen.from_namelist_xml(xml_fil)' components/blom//cime_config/buildnml
+fi
+if [ -z "$(grep NTASKS_PER_INST_OCN components/blom//cime_config/buildcpp)" ];then
+    sed -i -e's/NTASKS_OCN/NTASKS_PER_INST_OCN/' components/blom//cime_config/buildcpp
+fi
+
